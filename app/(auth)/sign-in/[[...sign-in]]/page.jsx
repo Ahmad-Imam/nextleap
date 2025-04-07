d586703dbe381663f5b44ec5cdbf7cbe7cbf0089
Ahmad-Imam <ahmadimam@iut-dhaka.edu>
2025-04-07 22:15:35 +0300

    clerk auth added

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -0,0 +1,13 @@
+import { SignIn } from "@clerk/nextjs";
+import React from "react";
+
+export default function SignInPage() {
+  return (
+    <div>
+      <SignIn
+        signInFallbackRedirectUrl="/"
+        // signInForceRedirectUrl="/onboarding"
+      />
+    </div>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div>
+        <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl gradient-title animate-gradient">
+          Take your career to the next level with
+          <br />
+          tracking and insights
+        </h1>
+      </div>
+    </div>
+  );
+}
